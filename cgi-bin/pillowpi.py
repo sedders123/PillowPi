d2048c1a76e9748a8f0c1d4d29e75c8339956c8d
sedders123 <sedders123@gmail.com>
2015-03-17 15:56:18 +0000

    Updated web interface, Implementing meta http refreshes

--- cgi-bin/pillowpi.py
+++ cgi-bin/pillowpi.py
@@ -21,22 +21,17 @@ def html_top():
         '''produces top and head section of the web page to be generated'''
         print(""" 
 	<!DOCTYPE html>
-	<html lang="en">
-    <head>
-	<meta charset="utf-8">
+<html lang="en">
+<head>
+  <meta charset="utf-8">
   <title>PillowPi - Web Interface!</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-
-	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
-	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
-	<!--script src="js/less-1.3.3.min.js"></script-->
-	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+  <meta http-equiv="refresh" content="10">
 	
 	<link href="/css/bootstrap.min.css" rel="stylesheet">
 	<link href="/css/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="/fonts/font-awesome-4.3.0/css/font-awesome.min.css">
 
   <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
   <!--[if lt IE 9]>
@@ -59,11 +54,7 @@ def html_top():
     <link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16">
     <link rel="manifest" href="favicons/manifest.json">
     <link rel="shortcut icon" href="favicons/favicon.ico">
-    
-        <!-- JavaScript Imports -->
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/scripts.js"></script>
+
 </head>
 	<body>
 	""")
@@ -72,35 +63,34 @@ def html_top():
 
 def html_body_top():
     print("""
-    <div class="container">
+<div class="container">
 	<div class="row clearfix">
 		<div class="col-md-12 column text-center">
 			<h2 class="title ">Pillow<span class="highlight">Pi</span></h2>
 			<p>
-				Welcome to the PillowPi Web Interface. Visit <a href="sedders123.github.io/PillowPi" target="_blank">our website</a> for full documentation 
+				Welcome to the PillowPi Web Interface. Visit <a href="https://sedders123.github.io/PillowPi" target="_blank">our website</a> for full documentation 
 			</p>
-            <div class="row clearfix">""")
+            <div class="row clearfix">
+            """)
     
 def html_body_main(light,temp):
     if light == "HIGH":
         print("""
-        <div class="col-md-6 column">
+            <div class="row clearfix">
+				<div class="col-md-6 column">
                     <h2>Light Levels</h2>
                     <div>
                         <div class="circle-red"><div class="title">Too Bright</div></div>
                     </div>
-                        
-				</div>
         """)
     elif light == "OK":
         print("""
-        <div class="col-md-6 column">
+            <div class="row clearfix">
+				<div class="col-md-6 column">
                     <h2>Light Levels</h2>
                     <div>
                         <div class="circle-green"><div class="title">OK</div></div>
                     </div>
-                        
-				</div>
         """)
     else:
         print("""
@@ -108,8 +98,7 @@ def html_body_main(light,temp):
                     <h2>Light Levels</h2>
                     <div>
                         <div class="circle-amber"><div class="title">ERROR</div></div>
-                    </div>
-                        
+                    </div>     
 				</div>
         """)
     if temp == "HIGH":
@@ -166,15 +155,13 @@ def main():
     print("Light: {}".format(light))
     print("Temperature: {}".format(temp))
     
-while __name__=="__main__":
+if __name__=="__main__":
         http_header()
         html_top()
-        Running = True
-        while Running:
-            main()
-            html_body_top()
-            html_body_main(light, temp)
-            html_body_bottom()
-            sleep(5)
+        main()
+        html_body_top()
+        html_body_main(light, temp)
+        html_body_bottom()
+        sleep(5)
 	
 	
